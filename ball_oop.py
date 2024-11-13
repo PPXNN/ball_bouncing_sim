daad65e57292ffcc52560b0de401b06d7508844a
Thapanan Suwansukhum <thapanansu555@gmail.com>
2024-11-13 14:20:52 +0700

    Activate the simulation

--- ball_oop.py
+++ ball_oop.py
@@ -1,4 +1,5 @@
 import turtle
+import random
 
 
 class Ball:
@@ -31,4 +32,57 @@ class Ball:
         if abs(self.y) > (canvas_height - self.size):
             self.vy = -self.vy
 
+class Simulation:
+    def __init__(self, num):
+        self.num_ball = num
+        self.canvas_width = turtle.screensize()[0]
+        self.canvas_height = turtle.screensize()[1]
+        self.ball_radius = 0.05 * self.canvas_width
+        self.balls = []
+        self.dt = 1
+        self.create()
 
+        turtle.speed(0)
+        turtle.tracer(0)
+        turtle.hideturtle()
+        turtle.colormode(255)
+
+    def create(self):
+        for i in range(self.num_ball):
+            x = random.uniform(-1*self.canvas_width + self.ball_radius, self.canvas_width - self.ball_radius)
+            y = random.uniform(-1*self.canvas_height + self.ball_radius, self.canvas_height - self.ball_radius)
+            vx = 10*random.uniform(-1.0, 1.0)
+            vy = 10*random.uniform(-1.0, 1.0)
+            color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+            self.balls.append(Ball(color, self.ball_radius, x, y, vx, vy))
+
+    def draw_border(self):
+        turtle.penup()
+        turtle.goto(-self.canvas_width, -self.canvas_height)
+        turtle.pensize(10)
+        turtle.pendown()
+        turtle.color((0, 0, 0))
+        for i in range(2):
+            turtle.forward(2 * self.canvas_width)
+            turtle.left(90)
+            turtle.forward(2 * self.canvas_height)
+            turtle.left(90)
+
+    def update(self):
+        turtle.clear()
+        self.draw_border()
+        for i in self.balls:
+            i.draw_ball()
+            i.move_ball(self.dt)
+            i.update_ball_velocity(self.canvas_width, self.canvas_height)
+        turtle.update()
+
+    def run(self):
+        while True:
+            self.update()
+
+num_balls = int(input("Number of balls to simulate: "))
+ball = Simulation(num_balls)
+ball.run()
+
+turtle.done()
